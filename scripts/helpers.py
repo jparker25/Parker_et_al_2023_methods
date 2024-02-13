80fabe6fd4f04eeb09d5ea9a1a0b8b0aa86abebe
John Parker <jep9223@gmail.com>
2024-02-12 21:35:01 -0500

    UPdated README.

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,25 +1,28 @@
 import os
 
+
+# Clean up axes
 def makeNice(axes):
     if type(axes) == list:
         for axe in axes:
-            for i in ['left','right','top','bottom']:
-                if i != 'left' and i != 'bottom':
+            for i in ["left", "right", "top", "bottom"]:
+                if i != "left" and i != "bottom":
                     axe.spines[i].set_visible(False)
-                    axe.tick_params('both', width=0,labelsize=8)
+                    axe.tick_params("both", width=0, labelsize=8)
                 else:
                     axe.spines[i].set_linewidth(3)
-                    axe.tick_params('both', width=0,labelsize=8)
+                    axe.tick_params("both", width=0, labelsize=8)
     else:
-        for i in ['left','right','top','bottom']:
-                if i != 'left' and i != 'bottom':
-                    axes.spines[i].set_visible(False)
-                    axes.tick_params('both', width=0,labelsize=8)
-                else:
-                    axes.spines[i].set_linewidth(3)
-                    axes.tick_params('both', width=0,labelsize=8)
+        for i in ["left", "right", "top", "bottom"]:
+            if i != "left" and i != "bottom":
+                axes.spines[i].set_visible(False)
+                axes.tick_params("both", width=0, labelsize=8)
+            else:
+                axes.spines[i].set_linewidth(3)
+                axes.tick_params("both", width=0, labelsize=8)
 
+
+# Run str on terminal and print out
 def run_cmd(str):
     print(str)
     os.system(str)
-        
